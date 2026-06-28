46b851e5c9e54b7e80fc00c718a00f468f2f3225
Qevin Attaqwa <qevinattaqwa.rx@gmail.com>
2026-06-28 22:07:11 +0700

    Update and rename Exploratory Data analysis Practice.sql to 02_exploratory_data_analysis.sql

--- sql/02_exploratory_data_analysis.sql
+++ sql/02_exploratory_data_analysis.sql
@@ -2,36 +2,36 @@
 
 
 select *
-from layoffs_stagging2;
+from layoffs_staging2;
 
 
 select max(total_laid_off), max(percentage_laid_off)
-from layoffs_stagging2;
+from layoffs_staging2;
 
 
 
 select *
-from layoffs_stagging2
+from layoffs_staging2
 where percentage_laid_off = 1
 order by total_laid_off desc;
 
 select *
-from layoffs_stagging2
+from layoffs_staging2
 where percentage_laid_off = 1
 order by funds_raised_millions desc;
 
 select company, sum(total_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 group by company
 order by 2 desc; 
 
 
 select min(`date`), max(`date`)
-from layoffs_stagging2;
+from layoffs_staging2;
 
 
 select industry, sum(total_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 group by industry
 order by 2 desc; 
 
@@ -41,13 +41,13 @@ from layoffs_stagging2;
 
 
 select country, sum(total_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 group by country
 order by 2 desc; 
 
 
 select year(`date`), sum(total_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 group by year(`date`)
 order by 2 desc; 
 
@@ -58,13 +58,13 @@ group by stage
 order by 2 desc; 
 
 select company, sum(percentage_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 group by company 
 order by 2 desc; 
 
 
 select substring(`date`,1,7) as `Month`, sum(total_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 where substring(`date`,1,7) is not null
 group by `Month`
 order by 1 asc;
@@ -73,7 +73,7 @@ order by 1 asc;
 with Rolling_Total as
 (
 select substring(`date`,1,7) as `Month`, sum(total_laid_off) as total_off
-from layoffs_stagging2
+from layoffs_staging2
 where substring(`date`,1,7) is not null
 group by `Month`
 order by 1 asc
@@ -84,12 +84,12 @@ from Rolling_Total;
 
 
 select company, sum(total_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 group by company
 order by 2 desc; 
 
 select company, Year(`date`), sum(total_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 group by company, Year(`date`)
 order by 3 asc;
 
@@ -97,7 +97,7 @@ order by 3 asc;
  with  company_Year (company, years, total_laid_off) as 
 (
 select company, Year(`date`), sum(total_laid_off)
-from layoffs_stagging2
+from layoffs_staging2
 group by company, Year(`date`)
 ), company_year_rank as
 
