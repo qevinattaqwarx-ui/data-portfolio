e25b3882479b9b4267a260231a75250abac46e72
Qevin Attaqwa <qevinattaqwa.rx@gmail.com>
2026-06-28 22:05:28 +0700

    Update and rename Data Cleaning Practice.sql to 01_data_cleaning.sql

--- sql/01_data_cleaning.sql
+++ sql/01_data_cleaning.sql
@@ -9,15 +9,15 @@ from layoffs;
     -- 4. remove any columns
 
 
-create table layoffs_stagging
+create table layoffs_staging
 like layoffs;
     
     
     
 select *
-from layoffs_stagging;
+from layoffs_staging;
 
-insert layoffs_stagging
+insert layoffs_staging
 select *
 from layoffs; 
 
@@ -26,7 +26,7 @@ from layoffs;
 select *, 
 row_number () over(
 partition by company,industry,total_laid_off,percentage_laid_off,`date`) as row_num  
-from layoffs_stagging;
+from layoffs_staging;
 
 
 with duplicate_cte as
@@ -36,14 +36,14 @@ row_number () over(
 partition by company,location,industry,
 			total_laid_off,percentage_laid_off,`date`, stage,
             country, funds_raised_millions) as row_num  
-from layoffs_stagging
+from layoffs_staging
 )
 select*
 from duplicate_cte
 where row_num > 1; 
 
 select *
-from layoffs_stagging
+from layoffs_staging
 where company = 'Casper';
 
 
@@ -54,7 +54,7 @@ row_number () over(
 partition by company,location,industry,
 			total_laid_off,percentage_laid_off,`date`, stage,
             country, funds_raised_millions) as row_num  
-from layoffs_stagging
+from layoffs_staging
 )
 delete 
 from duplicate_cte
@@ -63,7 +63,7 @@ where row_num > 1;
 
 
 
-CREATE TABLE `layoffs_stagging2`(
+CREATE TABLE `layoffs_staging2`(
   `company` text,
   `location` text,
   `industry` text,
@@ -79,27 +79,27 @@ CREATE TABLE `layoffs_stagging2`(
 
 
 select*
-from layoffs_stagging2;
+from layoffs_staging2;
 
-insert into layoffs_stagging2
+insert into layoffs_staging2
 select *, 
 row_number () over(
 partition by company,location,industry,
 			total_laid_off,percentage_laid_off,`date`, stage,
             country, funds_raised_millions) as row_num  
-from layoffs_stagging ;
+from layoffs_staging ;
 
 select*
-from layoffs_stagging2
+from layoffs_staging2
 where row_num > 1;
 
 delete 
-from layoffs_stagging2
+from layoffs_staging2
 where row_num > 1;
 
 
 select*
-from layoffs_stagging2;
+from layoffs_staging2;
 
 
 
@@ -107,88 +107,88 @@ from layoffs_stagging2;
 -- standardizing data 
 
 select company, trim(company)
-from layoffs_stagging2;
+from layoffs_staging2;
 
-update layoffs_stagging2
+update layoffs_staging2
 set company = trim(company);
 
 
 select *
-from layoffs_stagging2
+from layoffs_staging2
 where industry like 'Crypto%';
 
-update layoffs_stagging2
+update layoffs_staging2
 set industry = 'Crypto'
 where industry like 'Crypto%';
 
 select distinct industry
-from layoffs_stagging2;
+from layoffs_staging2;
 
 
 select distinct industry
-from layoffs_stagging2
+from layoffs_staging2
 order by 1;
 
 select distinct country, trim(trailing '.' from country)
-from layoffs_stagging2
+from layoffs_staging2
 order by 1 
 ;
 
-update layoffs_stagging2
+update layoffs_staging2
 set country = trim(trailing '.' from country)
 where country like 'United States%';
 
 
 select `date`, 
 STR_TO_DATE(`date`,'%m/%d/%Y')
-from layoffs_stagging2 
+from layoffs_staging2 
 ;
 
 
-update layoffs_stagging2 
+update layoffs_staging2 
 set `date` = STR_TO_DATE(`date`, '%m/%d/%Y')
 ;
 
 
 
 select `date`
-from layoffs_stagging2 
+from layoffs_staging2 
 ;
 
-alter table layoffs_stagging2
+alter table layoffs_staging2
 modify column `date`DATE;
 
 
 SELECT*
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE total_laid_off is null
 and percentage_laid_off is null;
 
-update layoffs_stagging2
+update layoffs_staging2
 set industry = null
 where industry = '';
 
 
 
 SELECT *
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE industry is null
 or industry = ''; 
 
 SELECT *
-FROM layoffs_stagging2
+FROM layoffs_staging2
 where company = 'Airbnb';
 
 SELECT *
-FROM layoffs_stagging2 t1
-join layoffs_stagging2 t2
+FROM layoffs_staging2 t1
+join layoffs_staging2 t2
 	on t1.company = t2.company
     and t1.location = t2.location 
 where(t1.industry is null or t1.industry = '')
 and t2.industry is not null;
 
-update layoffs_stagging2 t1
-join layoffs_stagging2 t2
+update layoffs_staging2 t1
+join layoffs_staging2 t2
 	on t1.company = t2.company
 set  t1.industry = t2.industry
 where t1.industry is null 
@@ -196,26 +196,26 @@ and t2.industry is not null;
 
 
 SELECT *
-FROM layoffs_stagging2
+FROM layoffs_staging2
 where company like 'Bally%';
 
 select *
-from layoffs_stagging2;
+from layoffs_staging2;
 
 
 SELECT*
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE total_laid_off is null
 and percentage_laid_off is null;
  
 
 delete 
-FROM layoffs_stagging2
+FROM layoffs_staging2
 WHERE total_laid_off is null
 and percentage_laid_off is null;
 
 
-alter table layoffs_stagging2
+alter table layoffs_staging2
 drop column row_num;
 
 
